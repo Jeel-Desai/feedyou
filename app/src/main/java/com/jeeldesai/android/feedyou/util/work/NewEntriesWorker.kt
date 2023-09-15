7289c34ff4d880287020aefde5f4b522ddfedc4b
Jeel Desai <jeeldesai@duck.com>
2023-09-15 21:13:53 +0530

    Revert "changed some UI elements and Icons"
    This reverts commit cac544f0f6f80f8bb7b71673d91feff0ef17fab2.

--- app/src/main/java/com/jeeldesai/android/feedyou/util/work/NewEntriesWorker.kt
+++ app/src/main/java/com/jeeldesai/android/feedyou/util/work/NewEntriesWorker.kt
@@ -72,7 +72,7 @@ class NewEntriesWorker(
 
         return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setTicker(resources.getString(com.jeeldesai.android.feedyou.R.string.new_entries_notification_title, feedTitle))
-            .setSmallIcon(com.jeeldesai.android.feedyou.R.drawable.feed_icon_small)
+            .setSmallIcon(com.jeeldesai.android.feedyou.R.drawable.ic_nicefeed_notif)
             .setContentTitle(resources.getString(com.jeeldesai.android.feedyou.R.string.new_entries_notification_title, feedTitle))
             .setStyle(NotificationCompat.BigTextStyle().bigText(text))
             .setContentText(text)
